598f610802fca2b39950a586471f203246bf5cbe
Giovanni Zaghi <vanni.freitas@hotmail.com>
2024-04-29 00:38:37 -0300

    fix: app listen

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup("api", app, document)
 
-    await app.listen(8080)
+    await app.listen(3000)
 
     console.log("Rodando ja em local host amigao > ")
 
