6b87b30fd4d32e7dc07001a7110e18df402471d5
Miguel Marques <miguel.marques@gamconsultoria.com.br>
2025-03-23 00:31:57 -0300

    feature: setting the schedule status to finished after sending feedback

--- src/controller/avaliation/avaliation.controller.ts
+++ src/controller/avaliation/avaliation.controller.ts
@@ -8,16 +8,28 @@ import {
 } from "@nestjs/common"
 import { AvaliationService } from "./avaliation.service"
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard"
-import { AgendaStatus } from "../scheduling/entities/scheduling.entity";
+import { AgendaStatus } from "../scheduling/entities/scheduling.entity"
+import { SchedulingService } from "../scheduling/scheduling.service"
 
 @Controller("avaliation")
 export class AvaliationController {
-    constructor(private readonly avaliationService: AvaliationService) { }
+    constructor(
+        private readonly avaliationService: AvaliationService,
+        readonly scheduleService: SchedulingService
+    ) {}
 
     @Post(":id/evaluate")
     async evaluateFreelancer(
         @Param("id") entrepreneurId: number,
-        @Body() evaluationData: { rating: number; comment: string, name: string, scheduleId: number, newStatus: AgendaStatus, userId: number }
+        @Body()
+        evaluationData: {
+            rating: number
+            comment: string
+            name: string
+            scheduleId: number
+            newStatus: AgendaStatus
+            userId: number
+        }
     ) {
         try {
             console.log("Trying to insert a avaliation")
@@ -31,6 +43,8 @@ export class AvaliationController {
                 evaluationData.userId
             )
 
+            await this.scheduleService.updateStatus(evaluationData.scheduleId, AgendaStatus.FINISHED);
+
             return { message: "Avaliação enviada com sucesso", avaliation }
         } catch (error) {
             if (error instanceof NotFoundException) {
@@ -39,5 +53,4 @@ export class AvaliationController {
             throw new Error("Falha ao enviar avaliação")
         }
     }
-
 }
