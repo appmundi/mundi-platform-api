fcec1f782dc9990eb058fdf190420524d6294935
Miguel Marques <linkmiguel@live.com>
2026-06-26 22:07:45 -0300

    adjustments in pnpm config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,33 +1,38 @@
-import { Injectable } from "@nestjs/common";
-import { createConnection, Connection } from "mysql2"; 
+import { Injectable } from "@nestjs/common"
+import {
+    createConnection,
+    type Connection,
+    type ConnectionOptions
+} from "mysql2/promise"
 
 @Injectable()
 export class AppService {
     async getHello(): Promise<any> {
-        const appStatus = "running";
-        let databaseStatus = "disconnected";
+        const appStatus = "running"
+        let databaseStatus = "disconnected"
 
-        const dbConfig = {
+        const dbConfig: ConnectionOptions = {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-        };
+            database: process.env.DB_DATABASE
+        }
 
-        const connection: Connection = createConnection(dbConfig);
+        let connection: Connection | undefined
 
         try {
-            await connection.connect();
-            databaseStatus = "connected";
+            connection = await createConnection(dbConfig)
+            await connection.ping()
+            databaseStatus = "connected"
         } catch (error) {
-            console.error("Error connecting to the database:", error);
+            console.error("Error connecting to the database:", error)
         } finally {
-            connection.end();
+            await connection?.end()
         }
 
         return {
             app: appStatus,
-            database: databaseStatus,
-        };
+            database: databaseStatus
+        }
     }
 }
